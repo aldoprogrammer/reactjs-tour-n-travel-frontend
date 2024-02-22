618ccb9702744bfd30cb859f426025bea1d748a5
aldoprogrammer <aldobesma@gmail.com>
2024-02-22 19:08:32 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
+import worldImg from '../assets/images/world.png'
+import { Col, Container, Row } from 'reactstrap'
+import Subtitle from '../shared/Subtitle'
+import '../styles/home.css';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return <>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6'>
+            <div className="hero__content">
+              <div className="hero__subtitle 
+              d-flex align-items-center">
+                <Subtitle subtitle={"Let's getting around this world"} />
+                <img src={worldImg} alt="" />
+                </div>
+                <h1>
+                  Enjoy how the nature will gives the best{" "}
+                  <span className='highlight'>memories</span>
+                </h1>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+          </Col>
+
+        </Row>
+      </Container>
+    </section>
+  </>
 }
 
 export default Home
