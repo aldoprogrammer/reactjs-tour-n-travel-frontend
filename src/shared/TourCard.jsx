2cf7b8a384511cf0548eaecac3b191af881303f1
aldoprogrammer <aldobesma@gmail.com>
2024-02-23 14:13:24 +0700

    update

--- src/shared/TourCard.jsx
+++ src/shared/TourCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card, CardBody } from 'reactstrap'
 import { Link } from 'react-router-dom'
+import './tour-card.css'
 const TourCard = ({ tour}) => {
     const { id, title, city, photo, price, featured, avgRating, reviews} = tour
 
